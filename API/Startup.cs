f167eac1697c5e5deb85642922feeffa8a97cc33
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2022-06-30 23:44:07 +0600

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,14 @@ namespace API
       {
         opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
       });
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+          policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+        });
+      });
+
     }
 
 
@@ -53,6 +61,7 @@ namespace API
       // app.UseHttpsRedirection();  
 
       app.UseRouting();
+      app.UseCors("CorsPolicy");
 
       app.UseAuthorization();
 
