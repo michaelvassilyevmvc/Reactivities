17ee7636fb7db36a8a89ad592ce9ee5605155147
vassilyev <vassilyev@toowto.kz>
2022-08-23 16:54:31 +0600

    End of section 12. Create Login, Register

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,71 +1,69 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Application.Activities;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using Persistence;
-using AutoMapper;
-using Application.Core;
 using API.Extensions;
 using FluentValidation.AspNetCore;
 using API.Middleware;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace API
 {
-  public class Startup
-  {
-    private readonly IConfiguration _config;
-    public Startup(IConfiguration config)
+    public class Startup
     {
-      _config = config;
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers(opt =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            })
+            .AddFluentValidation(config =>
+            {
+                config.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
+            services.AddApplicationServices(_config);
+            services.AddIdentityServices(_config);
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            if (env.IsDevelopment())
+            {
+                // app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            }
+
+            // app.UseHttpsRedirection();  
+
+            app.UseRouting();
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
-
-
-
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddControllers().AddFluentValidation(config =>
-      {
-        config.RegisterValidatorsFromAssemblyContaining<Create>();
-      });
-      services.AddApplicationServices(_config);
-    }
-
-
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-
-      app.UseMiddleware<ExceptionMiddleware>();
-
-      if (env.IsDevelopment())
-      {
-        // app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-      }
-
-      // app.UseHttpsRedirection();  
-
-      app.UseRouting();
-      app.UseCors("CorsPolicy");
-
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
-    }
-  }
 }
